5f9cda9eac56c367c8d438a745feab97f8d0dba0
enricoScholl <enrico.scholl.24@gmail.com>
2019-07-09 10:37:49 +0200

    Altri Vo

---
+++ golden_tour/src/it/golden_tour/entities/ProdottoVo.java
@@ -0,0 +1,21 @@
+package it.golden_tour.entities;
+
+public class ProdottoVo {
+
+	private Long id;
+	private String descrizione;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	
+}
