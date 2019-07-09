31fafa8190f8518a9772f0f31ab4e02102da52aa
enricoScholl <enrico.scholl.24@gmail.com>
2019-07-09 10:17:05 +0200

    classeVo ,get/set

--- golden_tour/src/it/golden_tour/entities/UtenteVo.java
+++ golden_tour/src/it/golden_tour/entities/UtenteVo.java
@@ -2,4 +2,48 @@ package it.golden_tour.entities;
 
 public class UtenteVo {
 
+	private Long id;
+	private Long tipologia;
+	private String nome;
+	private String cognome;
+	private String username;
+	private String password;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getTipologia() {
+		return tipologia;
+	}
+	public void setTipologia(Long tipologia) {
+		this.tipologia = tipologia;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
