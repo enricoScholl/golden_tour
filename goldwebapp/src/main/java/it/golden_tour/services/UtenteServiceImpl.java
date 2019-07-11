4915f3380e0ccd767fde4dbc7f8998ebb01b11b4
Admin <Admin@DESKTOP-ABC6HE6>
2019-07-11 10:45:49 +0200

    aggiunta from Vo to Dto e viceversa / package dto & utils

--- goldwebapp/src/main/java/it/golden_tour/services/UtenteServiceImpl.java
+++ goldwebapp/src/main/java/it/golden_tour/services/UtenteServiceImpl.java
@@ -3,8 +3,10 @@ package it.golden_tour.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import it.golden_tour.dto.UtenteDto;
 import it.golden_tour.entities.UtenteVo;
 import it.golden_tour.repo.UtenteRepository;
+import it.golden_tour.utils.UtenteUtils;
 
 @Service("utenteService")
 public class UtenteServiceImpl implements UtenteService {
@@ -18,8 +20,10 @@ public class UtenteServiceImpl implements UtenteService {
     }
 	
 	@Override
-    public UtenteVo login(String username, String password) throws Exception {
-		return utenteRepository.login(username, password);
+    public UtenteDto login(String username, String password) throws Exception {
+		
+		UtenteVo vo = utenteRepository.login(username, password);
+		return vo != null ? UtenteUtils.fromVoToDto(vo) : null;
        
     }
 
